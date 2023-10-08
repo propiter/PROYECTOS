f4aab3d8c61a2bb5cc53b045bbb3b2a0107bd71d
propiter <pro.pedro0413@gmail.com>
2023-10-08 14:02:56 -0500

    add

--- Sena/GA3-220501093-AA2-EV03/ejercicio2.js
+++ Sena/GA3-220501093-AA2-EV03/ejercicio2.js
@@ -1,1 +1,19 @@
 // edades
+const prompt = require("prompt-sync")();
+
+var edades = [];
+var loop = 0;
+var suma = 0;
+var menor = 0;
+var mayor = 0;
+var adulto = 0;
+var edad_alta = 0;
+var edad_baja = 0;
+var promedio = 0;
+
+while (loop < 10) {
+  let edad = prompt("digitar edad: ");
+  if (NaN(edad)) {
+    console.log("debe ser un valor numerico");
+  }
+}
