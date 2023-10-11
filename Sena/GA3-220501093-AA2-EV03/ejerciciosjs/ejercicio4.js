b2372a98ebea9a79d4f03a19c97c9f7a47d3aca1
propiter <pro.pedro0413@gmail.com>
2023-10-10 19:03:54 -0500

    Update ejercicio4.js

---
+++ Sena/GA3-220501093-AA2-EV03/ejerciciosjs/ejercicio4.js
@@ -0,0 +1,15 @@
+const prompt = require("prompt-sync")();
+
+var Personas = [];
+
+var nombre = "";
+var cedula = "";
+var celular = "";
+var email = "";
+var fechaNacimiento = "";
+var ciudadResidencia = "";
+var ciudadOrigen = "";
+var artista = "";
+var cancion1 = "";
+var cancion2 = "";
+var cancion3 = "";
